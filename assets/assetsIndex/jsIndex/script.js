706c4bfc6214f99b911fee7388201899b4a6fa80
Harsh Singh <codewithharshsingh@gmail.com>
2025-07-21 16:00:12 +0530

    updated freshcart project details

--- assets/assetsIndex/jsIndex/script.js
+++ assets/assetsIndex/jsIndex/script.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       img: "assets/assetsIndex/imagesIndex/freshcart-image.png",
       title: " FreshCart – Grocery Website",
-      desc: "A fully responsive grocery landing page featuring a clean design, mobile-friendly layout, and structured sections like header, hero, and product categories.",
+      desc: "Responsive grocery website clone with modern UI, mobile optimization, and clean design built using HTML, CSS, and JavaScript.",
       live: "https://freshcart-demo-site.netlify.app/",
       download:
         "https://github.com/codewithharshsingh/freshcart-demo-site/archive/refs/heads/main.zip",
